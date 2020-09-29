4279437dd17a90deef018c01cd48c8f7645416b6
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-29 07:20:12 +0530

    Tree using lists (Python builtin): access methods implemented.

--- Trees/TreeUsingLists.py
+++ Trees/TreeUsingLists.py
@@ -18,3 +18,20 @@ def insertRight(root, newBranch):
     else:
         root.insert(2, newBranch, [], [])
     return root
+
+
+def getRootVal(root):
+    return root[0]
+
+
+def setRootVal(root, newVal):
+    root[0] = newVal
+
+
+def getLeftChild(root):
+    return root[1]
+
+
+def getRightChild(root):
+    return root[2]
+
