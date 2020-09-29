5188a01f5a8d274b1da356f83942edfbc23f4923
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-29 07:39:30 +0530

    Tree using classes: access methods implemented.

--- Trees/Trees.py
+++ Trees/Trees.py
@@ -19,3 +19,15 @@ class BinaryTree(object):
             temp = BinaryTree(new_node)
             temp.right_child = self.right_child
             self.right_child = temp
+
+    def get_right_child(self):
+        return self.right_child
+
+    def get_left_child(self):
+        return self.left_child
+
+    def set_root_value(self, val):
+        self.value = val
+
+    def get_root_value(self):
+        return self.value
