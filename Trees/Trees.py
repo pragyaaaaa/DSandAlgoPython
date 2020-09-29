e22fb99eb17bbd2ffec9651a90eb5196be4597ae
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-29 07:36:03 +0530

    Tree using classes: class structure implemented.

---
+++ Trees/Trees.py
@@ -0,0 +1,16 @@
+class BinaryTree(object):
+    def __init__(self, root):
+        self.value = root
+        self.left_child = None
+        self.right_child = None
+
+    def insert_left_child(self, new_node):
+        if self.left_child == None:
+            self.left_child = BinaryTree(new_node)
+        else:
+            temp = BinaryTree(new_node)
+            temp.left_child = self.left_child
+            self.left_child = temp
+
+    def insert_right(self, new_node):
+        pass
