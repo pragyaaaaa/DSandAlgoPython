0a59180c68154a4f503d193067040c0f529c4708
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-30 07:40:59 +0530

    Graph Algorithms: DFS.

--- GraphAlgorithms/DFS.py
+++ GraphAlgorithms/DFS.py
@@ -1,8 +1,23 @@
 from collections import defaultdict
+
+
 class Graph:
     def __init__(self):
-        self.graph=defaultdict(list)
+        self.graph = defaultdict(list)
+
     def add_edge(self, u, v):
         self.graph[u].append(v)
-    def DFSUtil(selfself, v, visited):
-        visited
+
+    def dfs_util(self, v, visited):
+        visited[v] = True
+        print(v)
+        for i in self.graph[v]:
+            if not visited[i]:
+                self.dfs_util(i, visited)
+
+    def DFS(self):
+        V = len(self.graph)
+        visited = [False] * (V)
+        for i in range(V):
+            if not visited[i]:
+                self.dfs_util(i, visited)
