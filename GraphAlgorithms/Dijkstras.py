44497cf06087e0914ff6717e371ec0f8a2eb0d55
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-30 08:02:35 +0530

    Graph Algorithms: Added driver code for dijktras.

--- GraphAlgorithms/Dijkstras.py
+++ GraphAlgorithms/Dijkstras.py
@@ -8,18 +8,18 @@ class Graph:
 
     def print_solution(self, dist):
         print("Vertex distance from source: ")
-        for node in range(self.V):
-            print(node, "t", dist[node])
+        for node in range(self.v):
+            print(node, "to", dist[node])
 
     def min_distance(self, dist, spt_set):
         min_size = sys.maxsize
-        for v in range(self.V):
+        for v in range(self.v):
             if dist[v] < min_size and spt_set[v] == False:
                 min_size = dist[v]
                 min_index = v
         return min_index
 
-    def dijktra(self, src):
+    def dijkstra(self, src):
         dist = [sys.maxsize] * self.v
         dist[src] = 0
         spt_set = [False] * self.v
@@ -32,3 +32,19 @@ class Graph:
                         dist[v] > dist[u] + self.graph[u][v]:
                     dist[v] = dist[u] + self.graph[u][v]
         self.print_solution(dist)
+
+
+# driver code
+g = Graph(9)
+g.graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+           [4, 0, 8, 0, 0, 0, 0, 11, 0],
+           [0, 8, 0, 7, 0, 4, 0, 0, 2],
+           [0, 0, 7, 0, 9, 14, 0, 0, 0],
+           [0, 0, 0, 9, 0, 10, 0, 0, 0],
+           [0, 0, 4, 14, 10, 0, 2, 0, 0],
+           [0, 0, 0, 0, 0, 2, 0, 1, 6],
+           [8, 11, 0, 0, 0, 0, 1, 0, 7],
+           [0, 0, 2, 0, 0, 0, 6, 7, 0]
+           ];
+
+g.dijkstra(0);
