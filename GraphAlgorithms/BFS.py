b35e75e0b11048e44c3957858173a3e50e11741f
pragyaaaaa <pragyasabstracts@gmail.com>
2020-09-30 07:45:59 +0530

    Graph Algorithms: BFS.

---
+++ GraphAlgorithms/BFS.py
@@ -0,0 +1,21 @@
+from _collections import defaultdict
+
+
+class Graph:
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def add_edge(self, u, v):
+        self.graph[u].append(v)
+
+    def bfs(self, s):
+        visited = [False] * (len(self.graph))
+        queue = [s]
+        visited[s] = True
+        while queue:
+            s = queue.pop(0)
+            print(s, end=" ")
+            for i in self.graph[s]:
+                if not visited[i]:
+                    queue.append(i)
+                    visited[i] = True
